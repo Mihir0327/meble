607e690134136bc37e94e69f877c741a55e2e057
Mihir0327 <108055279+Mihir0327@users.noreply.github.com>
2023-02-27 11:53:55 +0530

    update 27 3

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,16 +3,21 @@ import MainSlider from "../component/mainSlider";
 import Summery from "../component/summery";
 import Discount from "../component/discount";
 import VideoPlayer from "../component/videoPlayer";
+import Blogs from "@/component/blogs";
+import Card from "@/component/Card";
+import Footer from "@/component/footer";
 
-const MainPage = () =>{
+const MainPage = () => {
     return (
         <>
-        <Header/>
-        <MainSlider/>
-        <Summery/>
-        <Discount/>
-        <VideoPlayer/>
-       
+            <Header />
+            <MainSlider />
+            <Summery />
+            <Discount />
+            <VideoPlayer />
+            {/* <Card /> */}
+            <Blogs />
+            <Footer />
         </>
     )
 
