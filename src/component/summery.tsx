f5d02c52aa8f9f73ececc24f0be3ed82f01c93ce
Mihir0327 <pathakmihir34@gmail.com>
2023-03-01 12:51:26 +0530

    update last

--- src/component/summery.tsx
+++ src/component/summery.tsx
@@ -5,8 +5,9 @@ const Summery = () => {
 
         <h1 className="text-xl  md:text-[2rem] font-display font-bold mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi?</h1>
         <h3 className="text-md font-normal">Lorem ipsum dolor, sit amet consectetur adipisicing elit. At a voluptate officiis facilis soluta alias minus error esse ipsa mollitia temporibus optio quos expedita, blanditiis quia pariatur. In possimus ullam exercitationem similique repellendus at quidem voluptas accusantium expedita, laboriosam nihil vero ratione illo, ipsa quos?Lorem ipsum dolor, sit amet consectetur adipisicing elit. At a voluptate officiis facilis soluta alias minus error esse ipsa mollitia temporibus optio quos expedita, blanditiis quia pariatur. In possimus ullam exercitationem similique repellendus at quidem voluptas accusantium expedita, laboriosam nihil vero ratione illo, ipsa quos?Lorem ipsum dolor, sit amet consectetur adipisicing elit. At a voluptate officiis facilis soluta alias minus error esse ipsa mollitia temporibus optio quos expedita, blanditiis quia pariatur. In possimus ullam exercitationem similique repellendus at quidem voluptas accusantium expedita, laboriosam nihil vero ratione illo, ipsa quos?</h3>
-
+        
         </div>
+        {/* <button className="">hello</button> */}
         </>
     )
 }
